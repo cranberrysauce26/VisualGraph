cb44c9a4ed2fa27f6ac3ed68e29685fe1fcd4d3e
Steven <stevenmai132@yahoo.ca>
2018-05-21 18:23:45 -0400

    1 tab -> 4 spaces

--- MVP/sandbox.py
+++ MVP/sandbox.py
@@ -1,14 +1,14 @@
 import resource
 
 standard_module_import_whitelist = ('math', 'random', 'time', 'datetime',
-                          'functools', 'itertools', 'operator', 'string',
-                          'collections', 're', 'json',
-'heapq', 'bisect', 'copy', 'hashlib')
+                                    'functools', 'itertools', 'operator', 
+                                    'string', 'collections', 're', 'json',
+                                    'heapq', 'bisect', 'copy', 'hashlib')
 
 custom_module_import_whitelist = ('graph',)
 
 for m in standard_module_import_whitelist+custom_module_import_whitelist:
-	__import__(m)
+    __import__(m)
 
 builtin_blacklist = ['reload', 'open', 'compile',
                    'file', 'eval', 'exec', 'execfile',
@@ -16,18 +16,18 @@ builtin_blacklist = ['reload', 'open', 'compile',
                    'globals', 'locals', 'vars']
 
 def restricted_import(*args):
-	# print("restricted_import with:", args, "hi")
-	# print("restricted import with args of type", type(args), args[0])
-	module_name = args[0]
-	print("module name is {0}".format(module_name))
-	if module_name in standard_module_import_whitelist + custom_module_import_whitelist:
-		imported_module = __import__(*args)
-		return imported_module
-	else:
-		raise Exception("{0} is a banned import".format(module_name))
+    # print("restricted_import with:", args, "hi")
+    # print("restricted import with args of type", type(args), args[0])
+    module_name = args[0]
+    print("module name is {0}".format(module_name))
+    if module_name in standard_module_import_whitelist + custom_module_import_whitelist:
+        imported_module = __import__(*args)
+        return imported_module
+    else:
+        raise Exception("{0} is a banned import".format(module_name))
 
 builtin_greylist = {
-	'__import__': restricted_import
+    '__import__': restricted_import
 }
 
 MAX_CPU_TIME = 3 # 3 seconds
@@ -37,36 +37,36 @@ MAX_MEMORY = 100000000 # 100 mega bytes
 
 # see https://docs.python.org/3/library/resource.html
 def set_resource_limits():
-	resource.setrlimit(resource.RLIMIT_NOFILE, (0,0))
-	resource.setrlimit(resource.RLIMIT_AS, (MAX_MEMORY, MAX_MEMORY))
-	resource.setrlimit(resource.RLIMIT_CPU, (MAX_CPU_TIME, MAX_CPU_TIME))
-	# pass
+    resource.setrlimit(resource.RLIMIT_NOFILE, (0,0))
+    resource.setrlimit(resource.RLIMIT_AS, (MAX_MEMORY, MAX_MEMORY))
+    resource.setrlimit(resource.RLIMIT_CPU, (MAX_CPU_TIME, MAX_CPU_TIME))
+    # pass
 
 
 def banned_builtin_wrapper(banned_function_name):
-	def wrapper(*args):
-		raise Exception(banned_function_name+ " is a banned builtin function")
-	return wrapper
+    def wrapper(*args):
+        raise Exception(banned_function_name+ " is a banned builtin function")
+    return wrapper
 
 # see this blog post http://mathamy.com/whats-the-deal-with-builtins-vs-builtin.html
 def safe_globals(builtin_main):
-	safe_builtins = {}
+    safe_builtins = {}
 
-	for key in dir(builtin_main):
-		val = getattr(builtin_main, key)
-		if key in builtin_blacklist:
-			safe_builtins[key] = banned_builtin_wrapper(key)
-		elif key in builtin_greylist:
-			safe_builtins[key] = builtin_greylist[key]
-		else:
-			safe_builtins[key] = val
+    for key in dir(builtin_main):
+        val = getattr(builtin_main, key)
+        if key in builtin_blacklist:
+            safe_builtins[key] = banned_builtin_wrapper(key)
+        elif key in builtin_greylist:
+            safe_builtins[key] = builtin_greylist[key]
+        else:
+            safe_builtins[key] = val
 
-	safe_globals = {
-		'__name__': '__main__',
-		'__builtins__': safe_builtins
-	}
+    safe_globals = {
+        '__name__': '__main__',
+        '__builtins__': safe_builtins
+    }
 
-	return safe_globals
+    return safe_globals
 
 if __name__== '__main__':
-	safe_globals(__builtins__)
+    safe_globals(__builtins__)
--- MVP/tracer.py
+++ MVP/tracer.py
@@ -4,52 +4,52 @@ import sandbox
 
 class Tracer(bdb.Bdb):
 
-	def __init__(self):
-		bdb.Bdb.__init__(self)
-		self.trace = []
+    def __init__(self):
+        bdb.Bdb.__init__(self)
+        self.trace = []
 
-	# main function
-	# it executes the code and returns the trace
-	def execute(self, code_str, builtins=__builtins__):
-		self.trace = []
-		# This causes errors for me!?!
-		# sandbox.set_resource_limits()
-		safe_globals = sandbox.safe_globals(builtins)
-		try:
-			self.run(code_str, safe_globals, safe_globals)
-		except Exception as e:
-			trace_entry = TraceEntry()
-			trace_entry.error = str(e)
-			self.trace = [trace_entry]
-			
+    # main function
+    # it executes the code and returns the trace
+    def execute(self, code_str, builtins=__builtins__):
+        self.trace = []
+        # This causes errors for me!?!
+        # sandbox.set_resource_limits()
+        safe_globals = sandbox.safe_globals(builtins)
+        try:
+            self.run(code_str, safe_globals, safe_globals)
+        except Exception as e:
+            trace_entry = TraceEntry()
+            trace_entry.error = str(e)
+            self.trace = [trace_entry]
+            
 
-	def user_return(self, frame, return_value):
-		if isinstance(return_value, TraceEntry):
-			# TODO: Make this less sketchy, maybe by iterating over the linked list
-			prev_frame = frame.f_back.f_back
-			assert(prev_frame)
-			return_value.line_number = prev_frame.f_lineno
-			self.trace.append(return_value)
+    def user_return(self, frame, return_value):
+        if isinstance(return_value, TraceEntry):
+            # TODO: Make this less sketchy, maybe by iterating over the linked list
+            prev_frame = frame.f_back.f_back
+            assert(prev_frame)
+            return_value.line_number = prev_frame.f_lineno
+            self.trace.append(return_value)
 
-	def user_line(self, frame):
-		pass
+    def user_line(self, frame):
+        pass
 
-	def user_call(self, frame, argument_list):
-		pass
+    def user_call(self, frame, argument_list):
+        pass
 
-	def user_exception(self, frame, exc_info):
-		error_entry = TraceEntry()
-		error_entry.error = exc_info[1]
-		self.trace = [error_entry]
-		raise bdb.BdbQuit
+    def user_exception(self, frame, exc_info):
+        error_entry = TraceEntry()
+        error_entry.error = exc_info[1]
+        self.trace = [error_entry]
+        raise bdb.BdbQuit
 
 if __name__ == '__main__':
-	tracer = Tracer()
-	code = open("example.py").read()
-	tracer.execute(code, __builtins__)
-	trace = tracer.trace
-	print("\nhere is the trace:")
-	for i, trace_entry in enumerate(trace):
-		print('trace[{0}]: '.format(i), end='')
-		trace_entry.display()
-		print(' ')
+    tracer = Tracer()
+    code = open("example.py").read()
+    tracer.execute(code, __builtins__)
+    trace = tracer.trace
+    print("\nhere is the trace:")
+    for i, trace_entry in enumerate(trace):
+        print('trace[{0}]: '.format(i), end='')
+        trace_entry.display()
+        print(' ')
