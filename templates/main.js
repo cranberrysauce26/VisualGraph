0ccba62ee462df4df0e444c36f4c50d519e26d7a
Howard Halim <howardhalim@gmail.com>
2018-05-23 15:23:42 -0400

    Why are traces being printed?

--- templates/main.js
+++ templates/main.js
@@ -1,27 +1,27 @@
-var graphManager = new GraphManager();
+// var graphManager = new GraphManager();
 
 function Visualize() {
     alert("Visualize Called");
-    var text = $("#textbox").val();
-    var dat = {code : text}
-    if (text.length == 0) {
-        document.getElementById("output").innerHTML = "You didn't enter anything!";
-        return;
-    }
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("Outputting");
-            document.getElementById("output").innerHTML = "Traces:<br>";
-            var str = this.responseText;
-            var traces = JSON.parse(str);
-            traces.forEach(function(trace){
-                // document.getElementById("output").innerHTML += "Hi "+JSON.stringify(trace) + "<br>";
-                // graphManager.CallFunction(trace);
-            });
-        }
-    };
-    xhttp.open("POST", "/trace", true);
-    xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send(JSON.stringify(dat));
+    // var text = $("#textbox").val();
+    // var dat = {code : text}
+    // if (text.length == 0) {
+    //     document.getElementById("output").innerHTML = "You didn't enter anything!";
+    //     return;
+    // }
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         alert("Outputting");
+    //         document.getElementById("output").innerHTML = "Traces:<br>";
+    //         var str = this.responseText;
+    //         var traces = JSON.parse(str);
+    //         traces.forEach(function(trace){
+    //             // document.getElementById("output").innerHTML += "Hi "+JSON.stringify(trace) + "<br>";
+    //             // graphManager.CallFunction(trace);
+    //         });
+    //     }
+    // };
+    // xhttp.open("POST", "/trace", true);
+    // xhttp.setRequestHeader("Content-Type", "application/json");
+    // xhttp.send(JSON.stringify(dat));
 }
