19572658971fa7beab940fb00ac6b4493381d39f
Steven <stevenmai132@yahoo.ca>
2018-05-25 00:12:50 -0400

    added random graph_id between 0 and large number

--- MVP/graph.py
+++ MVP/graph.py
@@ -1,51 +1,5 @@
 from tracer_entry import TraceEntry
-'''
-class Graph:
-    num_graphs=0
-
-    def _trace___init__(self, n):
-        tracer_entry = TraceEntry()
-        tracer_entry.graph_id = self.id
-        tracer_entry.args = [n]
-        tracer_entry.return_value = None
-        tracer_entry.command_name = "construct"
-        return tracer_entry
-
-    def _trace_add_vertex(self):
-        tracer_entry = TraceEntry()
-        tracer_entry.graph_id = self.id
-        tracer_entry.args = []
-        tracer_entry.return_value = None
-        tracer_entry.command_name = "add_vertex"
-        return tracer_entry
-
-    def _trace_add_edge(self, u, v):
-        tracer_entry = TraceEntry()
-        tracer_entry.graph_id = self.id
-        tracer_entry.args = [u, v]
-        tracer_entry.return_value = None
-        tracer_entry.command_name = "add_edge"
-        return tracer_entry
-
-    def __init__(self, n=0):
-        Graph.num_graphs = Graph.num_graphs + 1
-        self.id = Graph.num_graphs
-        self.n = n
-        self.adj = {i:{} for i in range(1, n+1)}
-        self._trace___init__(n)
-
-    def add_vertex(self):
-        self.n = self.n + 1
-        self.adj[self.n] = dict()
-        self._trace_add_vertex()
-
-    def add_edge(self, u, v):
-        if u < 0 or u > self.n or v < 0 or v > self.n:
-            raise Exception("invalid inputracer_entryt to add_edge")
-        self.adj[u][v]=1
-        self._trace_add_edge(u, v)
-'''
-
+import random
 class Vertex(dict):
     def __init__(self, id, graph):
         object.__setattr__(self, '_graph', graph)
@@ -67,12 +21,8 @@ class Vertex(dict):
             raise(Exception("You are not allowed to set property {0} of {1}".format(name, self.__class__)))
 
 class Graph:
-
-    num_graphs = 0
-
     def __init__(self, n=0, directed=False, weighted=False):
-        Graph.num_graphs = Graph.num_graphs + 1
-        self.id = Graph.num_graphs
+        self.id = random.randint(0, 1000000000000000000)
         self.num_vertices = n
         self.directed = directed
         self.weighted = weighted
