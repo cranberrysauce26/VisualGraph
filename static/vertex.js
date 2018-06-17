ae9368c9a304c3fc13a1d65ea293f7f5056e6327
Howard Halim <howardhalim@gmail.com>
2018-06-17 00:20:38 -0400

    A bit more code cleanup

--- static/vertex.js
+++ static/vertex.js
@@ -8,12 +8,4 @@ class Vertex {
     setProperty(name, value){
         this.properties[name] = value;
     }
-    //
-    // printProperties(position) {
-    //     var text = new paper.PointText(position);
-    //     text.justification = "center";
-    //     text.fillColor = 'black';
-    //     text.fontSize = '20px';
-    //     text.content = JSON.stringify(this.properties);
-    // }
 }
